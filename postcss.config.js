a86481d27f459f3332c1ea06a7ddb2c66a77d45d
youngboo <504142201@qq.com>
2021-09-19 17:57:04 +0800

    Initial commit

---
+++ postcss.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  plugins: [
+    // Plugins for PostCSS
+    "postcss-import",
+    "postcss-preset-env",
+  ],
+};
---
+++ src/layouts/SecurityLayout.jsx
@@ -0,0 +1,42 @@
+
+// import PageLoading from '@/components/PageLoading';
+import { stringify } from "qs";
+import {  Outlet,Navigate } from "react-router-dom";
+import { loginStateAtom } from '@/atoms/login';
+import { useRecoilValue } from 'recoil';
+import { useSafeState } from 'ahooks';
+
+
+
+const SecurityLayout = ({ children }) => {
+  // const [isReady] = useSafeState(false);
+  // const { children, loading, currentUser } = props; // You can replace it to your authentication rule (such as check token exists)
+  // 你可以把它替换成你自己的登录认证规则（比如判断 token 是否存在）
+
+
+  // const isLogin = currentUser && currentUser.userid;
+  const login = useRecoilValue(loginStateAtom);
+
+  const queryString = stringify({
+    redirect: window.location.href,
+  });
+
+  // if ((Login.isLogin && loading) || !isReady) {
+  //   return <PageLoading />;
+  // }
+
+  if (!login.isLogin && window.location.pathname !== "/user/login") {
+    return <Navigate to={`/user/login?${queryString}`} replace={true} />;
+    // return <Redirect from="window.location.href" to="/user/login" />;
+    // redirectTo(`/user/login?${queryString}`);
+  }
+
+
+  return (
+    <>
+      <Outlet />
+    </>
+  );
+}
+
+export default SecurityLayout;
